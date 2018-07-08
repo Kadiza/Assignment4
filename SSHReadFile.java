efc35c5d2f467a81a13a601c9b988e00268e3a27
kadiza <kadiza_ija@yahoo.com>
2018-07-09 02:04:53 +0800

    SSH program

---
+++ SSHReadFile.java
@@ -0,0 +1,42 @@
+import com.jcraft.jsch.*;
+import java.io.*;
+import java.util.Scanner;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+public class SSHReadFile {
+
+    public static void main(String args[]) {
+        String user = "root";
+        String password = "teror@1234";
+        String host = "192.168.43.123";
+        int port = 22;
+        String remoteFile = "/opt/iskandar/getme.txt";
+
+        try {
+            JSch jsch = new JSch();
+            Session session = jsch.getSession(user, host, port);
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+            System.out.println("Establishing Connection...");
+            session.connect();
+            System.out.println("Connection established.");
+            System.out.println("Crating SFTP Channel.");
+            ChannelSftp sftpChannel = (ChannelSftp) session.openChannel("sftp");
+            sftpChannel.connect();
+            System.out.println("SFTP Channel created.");
+
+            InputStream inputStream = sftpChannel.get(remoteFile);
+
+            try (Scanner scanner = new Scanner(new InputStreamReader(inputStream))) {
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    System.out.println(line);
+                }
+            }
+        } catch (JSchException | SftpException e) {
+            e.printStackTrace();
+        }
+    }
+}
